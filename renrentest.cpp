945995279a236392c05ae7b35e54bc9c415b9ea9
Gates Lu <lyjbbq@163.com>
2012-05-02 16:48:48 +0800

    Signed-off-by: Gates Lu <lyjbbq@163.com>

--- renrentest.cpp
+++ renrentest.cpp
@@ -59,6 +59,7 @@ renrentest::renrentest(const QString &m_sessionkey, QWidget *parent) :
     qDebug() << "m_sessionkey : " << m_sessionkey;
     qDebug()<<"main thread:" <<QThread::currentThreadId();
     on_tabWidget_currentChanged(0);
+    this->setWindowOpacity(0.95);          //ÉèÖÃÍ¸Ã÷
 }
 
 renrentest::~renrentest()
