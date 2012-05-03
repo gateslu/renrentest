5f721e60c0096628eeeebcd392a3edf38af7bf0f
Gates Lu <lyjbbq@163.com>
2012-05-03 09:59:33 +0800

    Signed-off-by: Gates Lu <lyjbbq@163.com>

--- main.cpp
+++ main.cpp
@@ -13,26 +13,18 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForCStrings(codec);
     QTextCodec::setCodecForLocale(codec);
 
-//        LoginDialog *loginDialog = new LoginDialog;
-//        if (loginDialog->exec() == QDialog::Accepted)
-//        {
-//            QString sessionKey = loginDialog->sessionKey();
+    LoginDialog *loginDialog = new LoginDialog;
+    if (loginDialog->exec() == QDialog::Accepted)
+    {
+        QString sessionKey = loginDialog->sessionKey();
 
-//            renrentest m_renrentest(sessionKey);
-//            m_renrentest.show();
+        renrentest m_renrentest(sessionKey);
+        m_renrentest.show();
 
-//            return a.exec();
-//        }
-//        else
-//        {
-//            return 0;
-//        }
-
-    //"6.c757661bf410e6d36599907b0380a22a.2592000.1335405600-448992126";
-    //"6.a99bb66a4b2e95ffa43d1506c8e7e200.2592000.1337446800-450631571"
-    renrentest m_renrentest("6.a99bb66a4b2e95ffa43d1506c8e7e200.2592000.1337446800-450631571");
-//    renrentest m_renrentest("6.c757661bf410e6d36599907b0380a22a.2592000.1335405600-448992126");
-    m_renrentest.show();
-
-    return a.exec();
+        return a.exec();
+    }
+    else
+    {
+        return 0;
+    }
 }
